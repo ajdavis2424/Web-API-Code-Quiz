3c7efc6b1e3b11b39f7b7cdddd28eb97d4be8bf8
ALexander Davis <ajdavis2424@gmail.com>
2021-07-19 10:59:19 -0400

    include jquery link to html, and practiced pseudo coing with tutor

--- code-quiz.js
+++ code-quiz.js
@@ -1,6 +1,48 @@
 // Click event creadted for the start button to load questions & hide buttin after clicking
-$(".btn-dark").on("click", function()){
+$(".btn-start").on("click", function(){
     // this removes start page from view
     $(".board").hide();
     console.log("user clicked start");
-}
+})
+
+var question = [
+
+]
+
+[// question: "blah blah"
+// answer 1 ""
+// answer 2 ""
+// answer 3 ""
+// answer 4 ""
+// ]
+// ]
+
+// question 2: "blah blah"
+// answer 1 ""
+// answer 2 ""
+// answer 3 ""
+// answer 4 ""
+
+// console.log(question)
+// console.log()
+
+// var timer = 75;
+// var score = 0;
+
+
+// function startTimer(question, time, score) {
+// jQuery.createQuestions(question);
+// jQuery.createTimer(time);
+// jQuery.createScore(score)
+// 
+// }
+
+// startTimer(questions, timer, score);
+
+// if questions  = 0 {
+    // timer === 0
+// }
+
+// if timer === 0 {
+    // prompt."enter your initials"
+    // }
