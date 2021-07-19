9141466972dac23d301bb68ecf6c726b0d057eda
ALexander Davis <ajdavis2424@gmail.com>
2021-07-18 21:49:09 -0400

    building CSS and JS pportion

---
+++ code-quiz.js
@@ -0,0 +1,6 @@
+// Click event creadted for the start button to load questions & hide buttin after clicking
+$(".btn-dark").on("click", function()){
+    // this removes start page from view
+    $(".board").hide();
+    console.log("user clicked start");
+}
