b9e489d8c1d86b868dceb3dbdff264bb7772265d
ALexander Davis <ajdavis2424@gmail.com>
2021-07-22 17:51:36 -0400

    more css styling

--- code-quiz.js
+++ code-quiz.js
@@ -1,13 +1,11 @@
 // Click event creadted for the start button to load questions & hide buttin after clicking
-$(".btn-start").on("click", function(){
+// $(".start-btn").on("click", function(){
     // this removes start page from view
-    $(".board").hide();
-    console.log("user clicked start");
-})
+    // $(".board").hide();
+    // console.log("user clicked start");
+// })
 
-var question = [
-
-]
+// var question = [
 
 [// question: "blah blah"
 // answer 1 ""
