820bb9fc1b69c0579c7047182e346559e8fc4299
ALexander Davis <ajdavis2424@gmail.com>
2021-07-24 23:05:41 -0400

    Javascript added, css mostly working

--- code-quiz.js
+++ code-quiz.js
@@ -1,3 +1,64 @@
+// establish all required elements-- CONST= ELEMENTS WILL ALWAYS PERFORM 1 ACTION
+const start-btn = document.querySelector(".start-btn button")
+const info-box = document.querySelector(".info-box")
+const exit-button = document.querySelector(".buttons .quit")
+const restart-btn = document.querySelector(".buttons .restart");
+
+// IF START BUTTON CLICKED... show info box
+start-btn.onclick = ()=>{
+    info-box.classlist.add("activeInfo"); 
+}
+// IF EXIT BUTTON CLICKED... hide info box
+exit-btn.onclick = ()=>{
+    info-box.classlist.remove("activeInfo");
+}
+
+// IF QUIT BUTTON CLICKED... hide info box
+exit-btn.onclick = ()=>{
+    info-box.classlist.remove("activeInfo");
+}
+
+// IF RESTART BUTTON CLICKED... hide info box & quiz box
+exit-btn.onclick = ()=>{
+    info-box.classlist.remove("activeQuiz");
+    quiz-box.classlist.remove("activeQuiz");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Click event creadted for the start button to load questions & hide buttin after clicking
 // $(".start-btn").on("click", function(){
     // this removes start page from view
